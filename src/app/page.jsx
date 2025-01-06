cbb5895203b7b0ff0d996bfa830ad24ae4e9d691
Aiden <aiden.t.oneal@gmail.com>
2025-01-05 18:51:42 -0600

    replaced gif with mp4

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -2,10 +2,18 @@ export default function Home() {
     return (
         <>
             <div className="main__cover">
-                <br/>
-                <h1><span className="mobile-disable">❋&nbsp;</span>Welcome to <span className="stroke">Azure Vision Records</span></h1>
-                <div className="main__cover-inner"></div>
+                <br />
+                <h1>
+                    <span className="mobile-disable">❋&nbsp;</span>
+                    Welcome to <span className="stroke">Azure Vision Records</span>
+                </h1>
+                <div className="main__cover-inner">
+                    <video className="background-video" autoPlay muted loop playsInline>
+                        <source src="/video/spinner.mp4" type="video/mp4" />
+                        Your browser does not support the video tag.
+                    </video>
+                </div>
             </div>
         </>
-    )
+    );
 }
